19d387ae9040c3287e7cfdf2ff5e85fd4d7a3d97
RiskyClick <jonn.thinkrtc@gmail.com>
2017-08-21 17:12:00 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './components/App'
-
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './reducers';
-
-
-const store = createStore(reducer);
-
 
 ReactDom.render(
-<Provider store={store}> 
-    <App />
-</Provider>, 
+    <div>Goal Achiever</div>
+    );
 
-document.querySelector(".container")
-);
