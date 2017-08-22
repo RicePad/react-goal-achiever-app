ab3659acf052f8f54bdaaf6933f73725d75db1d1
RiskyClick <jonn.thinkrtc@gmail.com>
2017-08-22 16:23:06 +0000

    add sign up page with bootstrap style

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 
 class SignUp extends Component {
+ constructor(props){
+  super(props);
+  
+  this.state = {
+    email: '',
+    password: ''
+   
+  };
+  
+ }
+ 
+ signUp(){
+  console.log('this.state', this.state);
+  
+ }
  
  
  render(){
      return(
-            <div> Sign Up from component</div>
+            <div className="form-inline">
+             <h1>Sign Up Page</h1>
+            <div className="form-group">
+                 <input
+                 className="form-control"
+                 type="text"
+                 placeholder="enter email"
+                 onChange={(event) => this.setState({email: event.target.value})}
+                 />
+                <input
+                 className="form-control"
+                 type="password"
+                 placeholder="enter password"
+                 onChange={(event) => this.setState({password: event.target.value})}
+                 />
+              <button
+               className="btn btn-successful"
+               onClick={() => this.signUp()}
+             > Sign Up</button>
+            </div>
+            
+            </div>
          );
      
  }   
